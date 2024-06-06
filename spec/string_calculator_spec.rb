13aae16a0ce9725ea28704a221b8abf2fcdeca6f
Kapil Patel <kapilppatel03@gmail.com>
2024-06-06 13:31:24 +0530

    added test case for addition of two values

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,5 +2,23 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
- 
+  describe 'performing addition' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+
+    context 'when given a single number inside the string' do
+      it 'return 1' do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+    end
+
+    context 'when given 2 comma seperated value' do
+      it 'return addition of that value' do
+        expect(StringCalculator.add("4,5")).to eq(9)
+      end
+    end
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,17 @@
 class StringCalculator
-		
+	def self.add(input)
+		return 0 if input.empty?
+		return 1 if input == "1"
+		return sum_two_numbers(input) if two_numbers_with_comma?(input)
+	end
+
+	private
+
+	def self.two_numbers_with_comma?(input)
+    input[0].to_i.positive? && input[1] == ',' && input[2].to_i.positive?
+  end
+
+	def self.sum_two_numbers(input)
+    input[0].to_i + input[2].to_i
+  end
 end
