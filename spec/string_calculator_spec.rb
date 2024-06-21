f7d3a82d7572feca3219086f26923e6041797a0d
Kapil Patel <kapilppatel03@gmail.com>
2024-06-21 17:15:03 +0530

    handled challenge tasks

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # spec/string_calculator_spec.rb
 require_relative '../string_calculator'
 
@@ -7,53 +5,55 @@ RSpec.describe StringCalculator do
   describe 'performing addition' do
     context 'when given an empty string' do
       it 'returns 0' do
-        expect(StringCalculator.add('')).to eq(0)
+        expect(StringCalculator.add("")).to eq(0)
       end
     end
 
     context 'when given a single number inside the string' do
       it 'return 1' do
-        expect(StringCalculator.add('1')).to eq(1)
+        expect(StringCalculator.add("1")).to eq(1)
       end
     end
 
     context 'when given 2 comma seperated value' do
       it 'return addition of that value' do
-        expect(StringCalculator.add('4,5')).to eq(9)
+        expect(StringCalculator.add("4,5")).to eq(9)
       end
     end
 
-    context 'when given multiple comma seperated values' do
-      it 'return sum of all values' do
-        expect(StringCalculator.add('4,6,0,8')).to eq(18)
+    context "when given multiple comma seperated values" do
+      it "return sum of all values" do
+        expect(StringCalculator.add("4,6,0,8")).to eq(18)
       end
     end
 
-    context 'when given new lines and comma separated values' do
-      it 'return sum of all values' do
+    context "when given new lines and comma separated values" do
+      it "return sum of all values" do
         expect(StringCalculator.add("1\n5,3,8")).to eq(17)
       end
     end
 
-    context 'when given value containing delimeter' do
-      it 'return sum of all values' do
+    context "when given value containing delimeter" do
+      it "return sum of all values" do
         expect(StringCalculator.add("//;\n1;2")).to eq(3)
       end
     end
 
-    context 'handle single negative value' do
-      it 'raises an error for negative numbers' do
-        expect do
-          StringCalculator.add('-1')
-        end.to raise_error('negative numbers not allowed please give positive number -1')
+    context "handle single negative value" do
+      it "raises an error for negative numbers" do
+        expect { StringCalculator.add("-1") }.to raise_error("negative numbers not allowed please give positive number -1")
       end
     end
 
-    context 'handle multiple negative value' do
-      it 'raises an error for negative numbers' do
-        expect do
-          StringCalculator.add("1\;-4\n-3")
-        end.to raise_error('negative numbers not allowed please give positive number -3')
+    context "handle multiple negative value" do
+      it "raises an error for negative numbers" do
+        expect { StringCalculator.add("1\;-4\n-3") }.to raise_error("negative numbers not allowed please give positive number -3")
+      end
+    end
+
+    context "hnadle custom delimeter multiple each value" do
+      it "return multiply of each value" do
+        expect(StringCalculator.add("**\n4*2*3")).to eq(24)
       end
     end
   end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,19 +1,21 @@
-# frozen_string_literal: true
-
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
 
     numbers = extract_numbers(input)
     validate_no_negative_numbers(numbers)
-
-    sum_of_values(numbers)
+    if custom_delimiter_with_estric(input)
+      value = multiply_values(numbers) 
+    else
+      value = sum_of_values(numbers)
+    end
+    value
   end
 
   private
 
   def self.extract_numbers(input)
-    if custom_delimiter?(input)
+    if custom_delimiter?(input) || custom_delimiter_with_estric(input)
       delimiter, numbers = extract_custom_delimiter(input)
       split_numbers(numbers, delimiter)
     else
@@ -21,13 +23,22 @@ class StringCalculator
     end
   end
 
+  def self.custom_delimiter_with_estric(input)
+    input.start_with?("**")
+  end
+
   def self.custom_delimiter?(input)
     input.start_with?("//")
   end
 
   def self.extract_custom_delimiter(input)
-    match = input.match(%r{//(.+)\n(.*)})
-    delimiter = Regexp.escape(match[1])
+    if input.start_with?('//')
+      match = input.match(%r{//(.+)\n(.*)})
+      delimiter = Regexp.escape(match[1])
+    else
+      match = input.match(%r{(.+)\n(.*)})
+      delimiter = Regexp.escape(match[1])
+    end
     numbers = match[2]
     [delimiter, numbers]
   end
@@ -43,5 +54,11 @@ class StringCalculator
   def self.validate_no_negative_numbers(values)
     negatives = values.map(&:to_i).select(&:negative?)
 		raise "negative numbers not allowed please give positive number #{negatives.join(', ')}" unless negatives.empty?
+  end
+
+  def self.multiply_values(values)
+    numeric_values = values.map { |value| value.split('*').map(&:to_i).reduce(1, :*) }
+
+    numeric_values.empty? ? 0 : numeric_values.reduce(1, :*)
   end
 end
